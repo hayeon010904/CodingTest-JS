330367993aa6168b320844c8895b8c2352a7b4ef
하염빵 <153696152+hayeon010904@users.noreply.github.com>
2024-01-28 18:06:07 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,10 @@
 const solution = (absolutes, signs) => {
+    let answer = 0;
  for(let i = 0; i<= signs.length-1; i++){
-!signs[i] ? absolutes[i] = (-absolutes[i]) : absolutes[i]
+!signs[i] ? answer += -absolutes[i] : answer += absolutes[i]
  }
-let result = absolutes.reduce((acc,cur) => acc+cur)
-return result
+return answer
 }
+// signs값이 true 이면=> 조건식 참
+// answer에다가 그대로/ false이면 -로 부호 변환해서 더해주기
+// let result = absolutes.reduce((acc,cur) => acc+cur)
