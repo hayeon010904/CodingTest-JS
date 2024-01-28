964c395c890dd8afc722179059558be48e90fa32
하염빵 <153696152+hayeon010904@users.noreply.github.com>
2024-01-28 16:37:21 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,25 @@
+const solution = (angle) => {
+    if(0<angle && angle<90) return 1;
+    else if (angle === 90) return 2;
+    else if(90< angle && angle<180) return 3;
+    else if(angle === 180) return 4;
+}
+
+
+
+// switch문은 정확한 값을 비교할 때만 용이함. 이외에는 ifelse
+// switch(angle){
+//         case 0 < angle < 90:
+//             return 1;
+//             break;
+//         case angle === 90:
+//             return 2;
+//             break;
+//         case 90 < angle < 180:
+//             return 3;
+//             break;
+//         case angle === 180:
+//             return 4;
+//             break;
+//      }
+
