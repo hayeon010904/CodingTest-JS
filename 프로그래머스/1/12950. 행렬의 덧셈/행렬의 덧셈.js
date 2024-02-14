a6a95864eb67be5b2fe17ab8cab23dbe5e5b456d
하염빵 <153696152+hayeon010904@users.noreply.github.com>
2024-02-15 02:02:59 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.31 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -14,3 +14,13 @@ if(arr1.length === arr2.length){
     }
 }
 return answer}
+//ex
+//바깥쪽 for문에서 i가 0일 때:
+//안쪽 for문에서 y가 0일 때: num = arr1[0][0] + arr2[0][0] → row.push(4)
+//안쪽 for문에서 y가 1일 때: num = arr1[0][1] + arr2[0][1] → row.push(6)
+//row는 [4, 6]이 되고, answer에 추가
+
+//바깥쪽 for문에서 i가 1일 때:
+//안쪽 for문에서 y가 0일 때: num = arr1[1][0] + arr2[1][0] → row.push(7)
+//안쪽 for문에서 y가 1일 때: num = arr1[1][1] + arr2[1][1] → row.push(9)
+//row는 [7, 9]가 되고, answer에 추가
