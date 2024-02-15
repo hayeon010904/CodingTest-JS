8edd0072ed2a9898761e212eca8ba94d66218854
하염빵 <153696152+hayeon010904@users.noreply.github.com>
2024-02-15 10:48:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.43 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,21 +1,18 @@
-//효율성에서 오답,,
 function solution(arr){
 let answer = []
 for(let i = 0; i<arr.length; i++){
-arr[i] === arr[i+1] ? '' : answer.push(arr[i])
+ if (arr[i] !== arr[i + 1]) {
+            answer.push(arr[i]);
+        }
 }
 return answer
 }
-//스택 큐 !
-// 큐; 줄서기 구조 활용???????????????
-// function solution(arr){
-// let answer = []
-// arr.forEach(i=> if(arr[i] === arr[i+1]){return arr.shift()})
 
 
-// return answer
+// function solution(arr)
+// {
+//     return arr.filter((val,index) => val != arr[index+1]);
 // }
-
 
 
    
